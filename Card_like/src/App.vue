b981432dbbf0501adf2181cf7f52d0bb396df2f9
Marksiranat <siranat2546@gmail.com>
2023-01-10 16:45:12 +0700

    first commit

---
+++ Card_like/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import LikeSad from "./components/LikeSad.vue";
+import IMG from "./components/img.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div v-for="n in 6" :key="n" class="col-4">
+        {{ n }}
+        <IMG></IMG>
+        <LikeSad></LikeSad>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ Card_like/src/components/img.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img
+      src="./moraine-lake-3840x2160-4k-5k-wallpaper-canada-mountains-lake-6190.jpg"
+      class="card-img-top"
+      alt="..."
+    />
+    <div class="card-body">
+      <p class="card-text">
+        Life is not about waiting for the storm to pass. It’s about learning to dance in the rain.
+      </p>
+    </div>
+  </div>
+</template>
